3869c7d51490b7c4771ce5f1ce1f3b53d81f220d
efolgar-2021545 <efolgar-2021545@kinal.edu.gt>
2026-06-21 23:42:26 -0600

    Se registraron las vistas Transfer y Favorites en el MainStack

--- KinalBankMovil/src/navigation/MainStack.jsx
+++ KinalBankMovil/src/navigation/MainStack.jsx
@@ -1,11 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainTabs from "./MainTabs";
+import NewTransactionScreen from "../features/transactions/screens/NewTransactionScreen";
+import FavoritesScreen from "../features/favorites/screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
 
 const MainStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="MainTabs" component={MainTabs} />
+    <Stack.Screen name="Transfer" component={NewTransactionScreen} />
+    <Stack.Screen name="Favorites" component={FavoritesScreen} />
   </Stack.Navigator>
 );
 
