b9d679c0ea7f4403c1c23cdd8f067085c4167e86
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-07-04 19:19:24 -0600

    Se arreglaron unos errores

--- BackendKinalBank/SistemaBancario/src/services/divisas-service.js
+++ BackendKinalBank/SistemaBancario/src/services/divisas-service.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 export const convertirMoneda = async (from, to, amount) => {
     const response = await axios.get(
         `${process.env.DIVISAS_API_URL}/api/divisas/convertir`,
-        { params: { from, to, amount } }
+        { params: { from, to, amount }, timeout: 15000 } // 15 segundos
     );
     return response.data;
 };
